35271035cca9248fb039c3f0d9111aed1d6bf589
J0schlZ <j0schlz@gmx.net>
2019-08-18 08:26:30 +0200

    Los gehts...

---
+++ CTSockets/src/main/java/de/crafttogether/CTSockets.java
@@ -0,0 +1,14 @@
+package de.crafttogether;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CTSockets extends JavaPlugin {
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+}
